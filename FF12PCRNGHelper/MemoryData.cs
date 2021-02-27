462b6af01f1a9537436884b84d63a6da2240c619
Nils Feierabend <nevigskcuforez@gmail.com>
2021-02-27 17:45:59 +0100

    use signature scanning for addresses

--- FF12PCRNGHelper/MemoryData.cs
+++ FF12PCRNGHelper/MemoryData.cs
@@ -1,17 +1,23 @@
 ﻿using System;
+using RFDown.AoB;
 
 namespace FF12PCRNGHelper
 {
     internal static class MemoryData
     {
-        // Is this actually static? Would be way faster since we don't have to rebase every rpm.
-        // If its not, maybe look into rebasing in AttachProc()
-        internal static IntPtr BaseAddress = new IntPtr(0x120000);
+        internal static readonly Signature MtiSig = new Signature("8B 15 ?? ?? ?? ?? 48 63 ?? 48 8D ?? ?? ?? ?? ?? FF C2 89 15 ?? ?? ?? ?? 8B 0C 81 8B C1 C1 E8 0B 33 C8 8B C1 25 ?? ?? ?? ?? C1 E0 07 33 C8 8B C1 25 ?? ?? ?? ?? C1 E0 0F 33 C8 8B C1 C1 E8 12 33 C1 48 83 C4 28", 2);
 
-        internal static IntPtr MtiAddress = BaseAddress + 0x2D94230;
+        internal const int MtSize = 624;
 
-        internal static int MtSize = 624;
+        internal static IntPtr BaseAddress;
 
-        internal static IntPtr MtAddress = MtiAddress - 4 * MtSize;
+        internal static IntPtr MtiAddress;
+        internal static IntPtr MtAddress;
+
+        internal static void LoadMtiAndMt(IntPtr mtiAddress)
+        {
+            MtiAddress = mtiAddress;
+            MtAddress = MtiAddress - (4 * MtSize);
+        }
     }
 }
